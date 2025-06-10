c5f228f8e75ccea542ba7ddf9664cf929d9b6ca7
shivateja8639 <shivateja8639@gmail.com>
2025-06-11 00:23:34 +0530

    Set up basic Node.js server with MVC structure and MongoDB integration
    - Created initial Express server
    - Connected MongoDB using Mongoose
    - Structured project using MVC pattern
    - Designed basic User model

---
+++ config/connectDb.js
@@ -0,0 +1,16 @@
+// connectDb.js
+const mongoose = require("mongoose");
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ MongoDB Connected");
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error);
+  }
+};
+
+module.exports = connectDb;
